f08bb160b4ebf543e761d6ec2c67ba272796aa5d
VishalG6 <vishalg_94@hotmail.com>
2016-01-05 23:08:49 +1100

    Initial commit

---
+++ app/src/main/java/com/example/vishal/animationstransitions/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.vishal.animationstransitions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
