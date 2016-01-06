f88ca70dd1a3fc53cf5396df456a4da16830cec4
VishalG6 <vishalg_94@hotmail.com>
2016-01-06 18:43:55 +1100

    Button Moved

--- app/src/main/java/com/example/vishal/animationstransitions/MainActivity.java
+++ app/src/main/java/com/example/vishal/animationstransitions/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.MotionEvent;
 import android.view.ViewGroup;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.transition.TransitionManager;
 
 
 public class MainActivity extends AppCompatActivity {
